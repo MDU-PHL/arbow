b03e827c00226d7264c6f93efe8f038f4eaf0456
Anders Goncalves da Silva <andersgs@gmail.com>
2020-04-26 15:39:54 -0700

    Fix automatic exit and add comments to warning lines

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 git update-index --refresh 
-git diff-index --quiet HEAD -- || (echo "Untracked changes..." && exit 1)
-echo "Before you deploy, have you:"
-echo " 1. Checked that the version number has been bumped, and commited?"
-echo " 2. Checked that the version of software in the README is correct, and commited any changes?"
-echo " If so, run bash deploy.sh | bash"
+git diff-index --quiet HEAD -- || echo "Untracked changes..."; exit 1
+echo "# Before you deploy, have you:"
+echo "# 1. Checked that the version number has been bumped, and commited?"
+echo "# 2. Checked that the version of software in the README is correct, and commited any changes?"
+echo "# If so, run bash deploy.sh | bash"
 version=$(cat arbow/version.py | cut -f2 -d "=" | sed -E 's/^[[:space:]]+"(.*)"/v\1/g')
 echo "git tag -a $version"
 echo "rm -rf build/* dist/*"
